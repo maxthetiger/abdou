d771b2a214a8917d1eaa44e32f186bd2c9efe24a
maxthetiger <maxthetiger@gmail.com>
2014-10-09 15:23:46 +0200

    update index + inc

---
+++ inc/circuits.php
@@ -0,0 +1,23 @@
+<div id="circuits">
+    <h2>Circuits</h2>
+    <p>2 circuits sont plus souvent pratiqués, car ils offrent une grande variété de sites intéressants. Cependant, je suis à votre disposition pour vous proposer d'autres destinations selon vos souhaits. Et, en cours de séjour, si la météo nous y oblige, nous pourrons bien sur modifier le parcours pour aller là où ça vole !</p>
+
+    <div class="agadir">
+        <h3>Agadir</h3>
+        <p>Transferts aéroport d'Agadir, vols littoral principalement, auberges pieds-dans-l'eau, jusqu'à 300m de dénivelé...</p>
+    </div>
+    <div class="marrakech">
+        <h3>Marrakech</h3>
+        <p>Transferts aéroport de Marrakech, vols en montagne, vue sur les sommets de l'Atlas, longs vols thermiques...</p>
+    </div>
+    <div class="check">
+        <ul>
+            <li>Disponible de septembre à janvier (de février à mai, contactez  <a href="http://www.lesgensdair.com/maroc.htm" title="Voyage de parapente au Maroc" target="_blank">Les Gens d'Air</a>).</li>
+            
+            <li>Pour voler avec moi, vous devez être un pilote breveté.</li>
+            
+            <li>Avant le départ, vous devez impérativement avoir rempli les conditions d'autorisation de vol au Maroc.</li>
+        </ul>
+    </div>
+</div>
+        
---
+++ inc/services.php
@@ -0,0 +1,34 @@
+<div id="services">
+    <h2>Services</h2>
+    Je me charge de l'intendance de votre séjour : transport, hôtels et gites, repas. Je vous servirai de guide et d'interprète
+    et je vous trouverai les meilleurs endroits pour que vous puissiez assouvir votre passion.<br />
+    <span class="tagline">Vous ne faites que gonfler, voler, manger, rire, dormir. Le reste, je m'en occupe...</span><br /><br />
+    <div id="serLeft">
+        <div class="transport">
+            <div class="serdisplay">
+               <h3>Transport</h3>
+                <p>Transfert de et vers l'aéroport, navettes entre l'attéro et le déco, courses locales avant le départ...</p>
+            </div>
+        </div>
+        <div class="repas">
+            <div class="serdisplay">
+                <h3>Repas</h3>
+                <p>Réservation dans des restaurants typiques, préparation des déjeuners directement sur le site, au besoin.</p>
+            </div>
+        </div>
+    </div>
+    <div id="serRight">
+        <div class="hebergement">
+            <div class="serdisplay">
+                <h3>Hébergement</h3>
+                <p>Réservations d'hôtels et d'auberges confortables, directement sur le déco lorsque possible.</p>
+            </div>
+        </div>
+        <div class="guide">
+            <div class="serdisplay">
+                <h3>Guide</h3>
+                <p>Proposition de circuits en fonction de l'aérologie, visites locales, renseignements culturels.</p>
+            </div>
+        </div>
+    </div>
+</div>
