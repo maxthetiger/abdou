9c42cad10c77f00dffbe177aa6ccb98de9ab5227
testusername <kiwimagine@gmail.com>
2014-10-09 15:06:15 +0200

    update jquery version

--- contact.php
+++ contact.php
@@ -144,7 +144,8 @@
 </div>
 
 
-<script type="text/javascript"  src="jquery-1.4.3.min.js"></script>
+<script type="text/javascript"  src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+
     
 <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.4.css" media="screen" />
 <script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
